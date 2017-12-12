046196b422267b6072de64af96b8e70483b47459
Gustavo Matozinho Lima <gustavomatozinho@gmail.com>
2017-12-12 14:38:19 -0200

    ajust

--- NormalDistribution/NormalDistribution/MainPage.cs
+++ NormalDistribution/NormalDistribution/MainPage.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using MathNet.Numerics;
+using MathNet.Numerics.Integration;
 using Xamarin.Forms;
 
 namespace NormalDistribution
@@ -15,28 +12,28 @@ namespace NormalDistribution
         {
             Content = BuildGrid(CalculateNormalDistribuitionValues());
         }
-        
+
         public Grid BuildGrid(List<double> list)
         {
             var grid = new Grid() { };
 
             for (var i = 0; i < 41; i++)
             {
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid.RowDefinitions.Add(new RowDefinition {Height = new GridLength(1, GridUnitType.Star)});
             }
 
             for (var i = 0; i < 11; i++)
             {
-                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition() {Width = new GridLength(1, GridUnitType.Star)});
                 if (i <= 0) continue;
-                var topName = new Label {Text = "" + (i-1)};
+                var topName = new Label {Text = "" + (i - 1)};
                 grid.Children.Add(topName, i, 0);
             }
 
             var pos = 0.0;
             for (var i = 1; i < 41; i++)
             {
-                var leftName = new Label { Text = "" + pos};
+                var leftName = new Label {Text = "" + pos};
                 pos += 0.1;
                 grid.Children.Add(leftName, 0, i);
             }
@@ -48,8 +45,8 @@ namespace NormalDistribution
                 for (var j = 1; j < grid.ColumnDefinitions.Count; j++)
                 {
                     if (list.Count == count) return grid;
-                    var test = new Label { Text = ""+list[count]};
-                    grid.Children.Add(test,j,i);
+                    var test = new Label {Text = "" + list[count]};
+                    grid.Children.Add(test, j, i);
                     count++;
                 }
             }
@@ -63,10 +60,10 @@ namespace NormalDistribution
         {
             double zScore;
             var list = new List<double>();
-            for (zScore=0; zScore < 3.99; zScore+=0.01)
+            for (zScore = 0; zScore < 3.99; zScore += 0.01)
             {
                 var result = CalculateNormalDistribuition(zScore);
-                list.Add(Math.Round(result,6));
+                list.Add(Math.Round(result, 6));
             }
 
             return list;
@@ -74,148 +71,31 @@ namespace NormalDistribution
 
         private double CalculateNormalDistribuition(double z)
         {
-            var integral = new DefiniteIntegral(x => somatorium(x,27), new Interval(0.0, z));
-            var aproximatte = integral.Approximate(DefiniteIntegral.ApproximationMethod.RectangleMidpoint, 1);
-            
-            
-            var result = aproximatte;
+
+            var firstPart = (1 / Math.Sqrt(2 * Math.PI));
+            var aproximatte = SimpsonRule.IntegrateComposite(x => CalcSomatorio(x,27), 0, z, 10000);
+            var result = firstPart * aproximatte;
 
             return result;
         }
 
-        private double somatorium(double x,double terms)
+        private double CalcSomatorio(double x, double terms)
         {
             double somatorio = 0;
-
-            var firstPart = (1 / Math.Sqrt(2 * Math.PI));
 
             for (var n = 0; n <= terms; n++)
             {
                 var alternateValue = Math.Pow(-1, n);
-                var numerador = (Math.Pow(x, 2 * n));
+                var numerador = (Math.Pow(x, 2 * n)) * alternateValue;
                 var firstPartDivision = (Math.Pow(2, n));
-                var secondPartDivision = fatorial(n);
+                var secondPartDivision = SpecialFunctions.Factorial(n);
                 var denominador = firstPartDivision * secondPartDivision;
                 var ztf = numerador / denominador;
-                var v = alternateValue * ztf;
-                somatorio += v;
+                somatorio += ztf;
             }
 
-            
-
-            return firstPart * somatorio;
-        }
-
-        public static Int32 fatorial(Int32 num)
-        {
-            var som = 1;
-            for (int i = 1; i <= num; i++)
-            {
-                som *= i;
-
-            }
-
-            return som;
+            return somatorio;
         }
 
     }
-
-    public class Interval
-    {
-        public Interval(double leftEndpoint, double size)
-        {
-            LeftEndpoint = leftEndpoint;
-            RightEndpoint = leftEndpoint + size;
-        }
-
-        public double LeftEndpoint
-        {
-            get;
-            set;
-        }
-
-        public double RightEndpoint
-        {
-            get;
-            set;
-        }
-
-        public double Size
-        {
-            get
-            {
-                return RightEndpoint - LeftEndpoint;
-            }
-        }
-
-        public double Center
-        {
-            get
-            {
-                return (LeftEndpoint + RightEndpoint) / 2;
-            }
-        }
-
-        public IEnumerable<Interval> Subdivide(int subintervalCount)
-        {
-            double subintervalSize = Size / subintervalCount;
-            return Enumerable.Range(0, subintervalCount).Select(index => new Interval(LeftEndpoint + index * subintervalSize, subintervalSize));
-        }
-    }
-
-    public class DefiniteIntegral
-    {
-        public DefiniteIntegral(Func<double, double> integrand, Interval domain)
-        {
-            Integrand = integrand;
-            Domain = domain;
-        }
-
-        public Func<double, double> Integrand
-        {
-            get;
-            set;
-        }
-
-        public Interval Domain
-        {
-            get;
-            set;
-        }
-
-        public double SampleIntegrand(ApproximationMethod approximationMethod, Interval subdomain)
-        {
-            switch (approximationMethod)
-            {
-                case ApproximationMethod.RectangleLeft:
-                    return Integrand(subdomain.LeftEndpoint);
-                case ApproximationMethod.RectangleMidpoint:
-                    return Integrand(subdomain.Center);
-                case ApproximationMethod.RectangleRight:
-                    return Integrand(subdomain.RightEndpoint);
-                case ApproximationMethod.Trapezium:
-                    return (Integrand(subdomain.LeftEndpoint) + Integrand(subdomain.RightEndpoint)) / 2;
-                case ApproximationMethod.Simpson:
-                    return (Integrand(subdomain.LeftEndpoint) + 4 * Integrand(subdomain.Center) + Integrand(subdomain.RightEndpoint)) / 6;
-                default:
-                    throw new NotImplementedException();
-            }
-        }
-
-        public double Approximate(ApproximationMethod approximationMethod, int subdomainCount)
-        {
-            return Domain.Size * Domain.Subdivide(subdomainCount).Sum(subdomain => SampleIntegrand(approximationMethod, subdomain)) / subdomainCount;
-        }
-
-        public enum ApproximationMethod
-        {
-            RectangleLeft,
-            RectangleMidpoint,
-            RectangleRight,
-            Trapezium,
-            Simpson
-        }
-    }
-
 }
-
