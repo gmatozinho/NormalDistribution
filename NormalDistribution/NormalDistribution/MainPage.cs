1dabee4e4726e354ae8441c8353a23cf0a917eb6
Gmatozinho <gustavomatozinho@gmail.com>
2018-06-14 19:29:54 -0300

    init

--- NormalDistribution/NormalDistribution/MainPage.cs
+++ NormalDistribution/NormalDistribution/MainPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using MathNet.Numerics;
 using MathNet.Numerics.Integration;
 using Xamarin.Forms;
@@ -13,11 +14,11 @@ namespace NormalDistribution
             Content = BuildGrid(CalculateNormalDistribuitionValues());
         }
 
-        public Grid BuildGrid(List<double> list)
+        public Grid BuildGrid(List<decimal> list)
         {
             var grid = new Grid() { };
 
-            for (var i = 0; i < 41; i++)
+            for (var i = 0; i < 32; i++)
             {
                 grid.RowDefinitions.Add(new RowDefinition {Height = new GridLength(1, GridUnitType.Star)});
             }
@@ -26,12 +27,12 @@ namespace NormalDistribution
             {
                 grid.ColumnDefinitions.Add(new ColumnDefinition() {Width = new GridLength(1, GridUnitType.Star)});
                 if (i <= 0) continue;
-                var topName = new Label {Text = "" + (i - 1)};
+                var topName = new Label {Text = "" + (i - 1)+ "π" };
                 grid.Children.Add(topName, i, 0);
             }
 
             var pos = 0.0;
-            for (var i = 1; i < 41; i++)
+            for (var i = 1; i < 32; i++)
             {
                 var leftName = new Label {Text = "" + pos};
                 pos += 0.1;
@@ -56,27 +57,31 @@ namespace NormalDistribution
         }
 
         //Gera a lista com todos os valores de distribuição normal calculados com precisão de 6 casas decimais
-        public List<double> CalculateNormalDistribuitionValues()
+        public List<decimal> CalculateNormalDistribuitionValues()
         {
             double zScore;
-            var list = new List<double>();
-            for (zScore = 0; zScore < 3.99; zScore += 0.01)
+            var list = new List<decimal>();
+            var oldzscore = 0.0;
+            for (zScore = 0; zScore < 3*Math.PI; zScore += 0.01*Math.PI)
             {
-                var result = CalculateNormalDistribuition(zScore);
-                list.Add(Math.Round(result, 6));
+                var result = CalculateNormalDistribuition(oldzscore,zScore);
+                list.Add((decimal)Math.Round(result, 6));
+                oldzscore = zScore;
             }
 
             return list;
         }
 
         //Calcula um valor especifico de distribuição normal multiplicando a constante pela integral da série
-        private double CalculateNormalDistribuition(double z)
+        private decimal CalculateNormalDistribuition(double init,double z)
         {
 
-            var firstPart = (1 / Math.Sqrt(2 * Math.PI));
-            var integrate = SimpsonRule.IntegrateComposite(x => CalcSomatorio(x,27), 0, z, 10000);
-            var result = firstPart * integrate;
-
+            //var firstPart = (1 / Math.Sqrt(2 * Math.PI));
+            var integrate = SimpsonRule.IntegrateComposite(x => CalcSomatorio(x,15), init, z, 10000);
+            //var result = firstPart * integrate;
+            decimal result = (decimal)integrate;
+            result = decimal.Parse(result.ToString(), NumberStyles.Float);
+          
             return result;
         }
 
@@ -90,12 +95,15 @@ namespace NormalDistribution
             {
                 var alternateValue = Math.Pow(-1, n);
                 var numerador = (Math.Pow(x, 2 * n)) * alternateValue;
-                var firstPartDivision = (Math.Pow(2, n));
-                var secondPartDivision = SpecialFunctions.Factorial(n);
-                var denominador = firstPartDivision * secondPartDivision;
+                //var firstPartDivision = (Math.Pow(2, n));
+                //var secondPartDivision = SpecialFunctions.Factorial(n);
+                //var denominador = firstPartDivision * secondPartDivision;
+                var denominador = SpecialFunctions.Factorial(2 * n + 1);
                 var ztf = numerador / denominador;
+                
                 somatorio += ztf;
             }
+
 
             return somatorio;
         }
